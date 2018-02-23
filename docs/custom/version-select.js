634b987420e29445cedab2982cc427398311e2af
Willet Vary <willetvary@gmail.com>
2018-02-23 10:48:50 -0800

    Fix run script (#224)
    * prevent removing of node_modules and site when running git clean
    
    * fixed logic to determine version in url
    
    * fixed create symbolic link
    
    * Updated run script
    
    * Updated regex match
    
    * Update version-select.js

--- docs/custom/version-select.js
+++ docs/custom/version-select.js
@@ -25,7 +25,7 @@ function initializeVersionSelection() {
   var REL_BASE_URL = (typeof base_url === 'undefined' ? '.' : base_url);
   var ABS_BASE_URL = normalizePath(window.location.pathname + "/" +
                                    REL_BASE_URL);
-  var CURRENT_VERSION = ABS_BASE_URL.split("/v/")[1] || '';
+  var CURRENT_VERSION = (ABS_BASE_URL.match(/^(\/v\/)([A-Za-z0-9\.\-_]+)(\/)/i) || [])[2] || '';
 
   function makeSelect(options, selected) {
     var select = document.createElement("select");
